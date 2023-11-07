ff2b7372355be8c51d55dc0f845147b5461a82a5
Malik Samansya <maliksamansya206@gmail.com>
2023-11-07 19:55:45 +0700

    from

--- instagram-clone/src/_auth/forms/SignupForm.tsx
+++ instagram-clone/src/_auth/forms/SignupForm.tsx
@@ -1,6 +1,8 @@
-import { TextInput, Checkbox, Button, Group, Box } from "@mantine/core";
+import { TextInput, Box, Image, Text, Button, Loader } from "@mantine/core";
 import { useForm } from "@mantine/form";
+import imageLogo from "./../../assets/images/image-logo.svg";
 import classes from "./SignupForm.module.css";
+import { Link } from "react-router-dom";
 const SignupForm = () => {
   const form = useForm({
     initialValues: {
@@ -13,17 +15,81 @@ const SignupForm = () => {
     },
   });
 
+  const isUserLoading = false;
+
   return (
-    <Box className={classes.container}>
-      <form onSubmit={form.onSubmit((values) => console.log(values))}>
-        <TextInput
-          withAsterisk
-          label="Email"
-          placeholder="your@email.com"
-          {...form.getInputProps("email")}
-        />
-      </form>
-    </Box>
+    <>
+      <Box className={classes.container}>
+        <Image src={imageLogo} alt="logo" />
+        <Text className={classes.accountText}>Create a new account</Text>
+        <Text className={classes.detailText}>
+          To use snapgram, Please enter your details
+        </Text>
+
+        <form
+          onSubmit={form.onSubmit((values) => console.log(values))}
+          className={classes.formContainer}
+        >
+          <TextInput
+            withAsterisk
+            label="Name"
+            name="name"
+            type="text"
+            {...form.getInputProps("name")}
+            classNames={{ input: classes.input }}
+            styles={{ label: { color: "white" } }}
+          />
+          <TextInput
+            withAsterisk
+            label="Username"
+            name="username"
+            type="text"
+            classNames={{ input: classes.input }}
+            styles={{ label: { color: "white" } }}
+            {...form.getInputProps("username")}
+          />
+          <TextInput
+            withAsterisk
+            label="Email"
+            name="email"
+            type="text"
+            classNames={{ input: classes.input }}
+            styles={{ label: { color: "white" } }}
+            {...form.getInputProps("email")}
+          />
+          <TextInput
+            withAsterisk
+            label="Password"
+            name="password"
+            type="text"
+            classNames={{ input: classes.input }}
+            styles={{ label: { color: "white" } }}
+            {...form.getInputProps("password")}
+          />
+
+          <Button
+            type="submit"
+            className={classes.buttonSubmit}
+            leftSection={
+              isUserLoading ? (
+                <Loader size={14} color="rgba(255, 255, 255, 0.95)" />
+              ) : (
+                ""
+              )
+            }
+          >
+            {isUserLoading ? "Loading..." : "Sign up"}
+          </Button>
+
+          <Text className={classes.logInText}>
+            Already have an account?
+            <Link to="/sign-in" className={classes.linkText}>
+              Log in
+            </Link>
+          </Text>
+        </form>
+      </Box>
+    </>
   );
 };
 
