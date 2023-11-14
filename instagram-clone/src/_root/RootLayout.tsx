225795d0d6ceff0794f0d11cb43fc2b839269edb
Malik Samansya <maliksamansya206@gmail.com>
2023-11-14 15:07:39 +0700

    create UI for Topbar

--- instagram-clone/src/_root/RootLayout.tsx
+++ instagram-clone/src/_root/RootLayout.tsx
@@ -1,8 +1,12 @@
-import { Button, Box } from "@mantine/core";
+import { Box, Flex, Image } from "@mantine/core";
 import { useEffect } from "react";
 import { useUserContext } from "./../context/AuthContext";
 import { Link, useNavigate } from "react-router-dom";
 import { useSignOutAccount } from "../lib/react-query/queriesAndMutations";
+import imageLogo from "./../assets/images/image-logo.svg";
+import iconLogout from "./../assets/icons/icon-logout.svg";
+import iconPlaceholder from "./../assets/icons/icon-profile-placeholder.svg";
+import classes from "./RootLayout.module.css";
 const RootLayout = () => {
   const navigate = useNavigate();
   const { user } = useUserContext();
@@ -14,12 +18,29 @@ const RootLayout = () => {
 
   return (
     <>
-      <Button color="lime" fullWidth>
+      {/* <Button color="lime" fullWidth>
         Button
-      </Button>
+      </Button> */}
       <Box style={{ width: "100%" }}>
         {/* Topbar */}
-        <Box></Box>
+        <Box className={classes.topbar}>
+          <Flex align="center" justify="space-between" py="1rem" px="1.25rem">
+            <Link to="/" className={classes.link}>
+              <Image src={imageLogo} w={130} alt="logo" />
+            </Link>
+
+            <Flex gap="1rem">
+              <Image src={iconLogout} alt="logout" onClick={() => signOut} />
+              <Link to={`/profile/${user.id}`} className={classes.link}>
+                <Image
+                  src={user.imageUrl || iconPlaceholder}
+                  alt="profile"
+                  className={classes.profileImg}
+                />
+              </Link>
+            </Flex>
+          </Flex>
+        </Box>
       </Box>
       {/* <div className="w-full md:flex">
       <Topbar />
